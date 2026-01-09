80ddfb3738c2b135e9e0bfb870f8241e9b701c91
sivarammalayappan-code <sivarammalayappan@gmail.com>
2026-01-10 03:06:50 +0530

    Test

--- Pythonfromscratch.py
+++ Pythonfromscratch.py
@@ -197,3 +197,16 @@ you are 32 years old and your weight is 88.5 kg
 print(f"Dear {name} , \nyou are {age} years old and your weight is {weight} kg ")
 
 ###############################
+project 3
+
+#TIP CALCULATOR
+
+print("Welcome to the tip calculator!")
+bill=float(input("Enter the Bill amount : Rs "))
+tip_percent=float(input("How much % you want to tip (10,12,15) : " ))
+total_tip=(bill*tip_percent/100)
+total_bill=bill+total_tip
+print(f"Your total bill is {total_bill:.2f}")                              #.2f rounds the decimal to two place
+no_of_person=int(input("How many people to share the bill : "))
+final_amount=total_bill/no_of_person
+print(f"Each person should pay {final_amount:.2f}")
