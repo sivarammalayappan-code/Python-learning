5f5a04ab5786bccc57f7bbf8e035dd38ac5de162
sivarammalayappan-code <sivarammalayappan@gmail.com>
2026-01-10 03:11:29 +0530

    Test

--- Pythonfromscratch.py
+++ Pythonfromscratch.py
@@ -209,4 +209,6 @@ total_bill=bill+total_tip
 print(f"Your total bill is {total_bill:.2f}")                              #.2f rounds the decimal to two place
 no_of_person=int(input("How many people to share the bill : "))
 final_amount=total_bill/no_of_person
-print(f"Each person should pay {final_amount:.2f}")
+print(f"Each person should pay Rs {final_amount:.2f}")
+
+##############################################################################################
